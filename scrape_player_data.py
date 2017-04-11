8a050f389b5cfa74d58b95cfa24d89b322f423e2
Rahul <rahul444@hotmail.com>
2017-04-10 22:54:46 -0700

    basic stats scraping

--- scrape_player_data.py
+++ scrape_player_data.py
@@ -1,7 +1,35 @@
 import bs4 as bs
 import urllib.request
+import requests
 
-URL = ""
+# URL = "https://pythonprogramming.net/parsememcparseface/"
+URL = "http://www.pro-football-reference.com/players/"
 
 source = urllib.request.urlopen(URL).read()
-print(source)
+soup = bs.BeautifulSoup(source, 'lxml')
+
+def get_player_stats(first, last):
+    last = last[:4]
+    if len(last) < 4:
+        last = last + ("x" * (4 - len(last)))
+    first = first[:2]
+
+    suffix = "00"
+    path = last[0] + "/" + last + first + suffix + ".htm"
+
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+    # r = requests.get(URL+path, headers=headers)
+    # data = r.text
+    # soup = bs.BeautifulSoup(data, "lxml")
+    # print(soup.find_all('div', class_='table_wrapper table_controls'))
+    
+    print(URL + path)
+    
+    source = urllib.request.urlopen(URL + path).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    print(soup.text)
+    # print(soup.find_all('div', class_='table_wrapper table_controls'))
+
+
+get_player_stats("Tom", "Brady")
