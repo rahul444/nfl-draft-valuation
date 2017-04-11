5350364445b8df204b4f683b9f607fa109fc0476
Rahul <rahul444@hotmail.com>
2017-04-11 01:42:50 -0700

    scrape player position from site

--- scrape_player_data.py
+++ scrape_player_data.py
@@ -2,13 +2,10 @@ import bs4 as bs
 import urllib.request
 import requests
 
-# URL = "https://pythonprogramming.net/parsememcparseface/"
-URL = "http://www.pro-football-reference.com/players/"
+BASE_URL = "http://www.pro-football-reference.com/players/"
 
-source = urllib.request.urlopen(URL).read()
-soup = bs.BeautifulSoup(source, 'lxml')
-
-def get_player_stats(first, last):
+def get_url(name):
+    first, last = name.split()
     last = last[:4]
     if len(last) < 4:
         last = last + ("x" * (4 - len(last)))
@@ -16,20 +13,29 @@ def get_player_stats(first, last):
 
     suffix = "00"
     path = last[0] + "/" + last + first + suffix + ".htm"
+    return BASE_URL + path
+
+
+def get_player_stats(name):
+    URL = get_url(name)
+    print(URL)
 
     # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
     # r = requests.get(URL+path, headers=headers)
-    # data = r.text
-    # soup = bs.BeautifulSoup(data, "lxml")
-    # print(soup.find_all('div', class_='table_wrapper table_controls'))
     
-    print(URL + path)
-    
-    source = urllib.request.urlopen(URL + path).read()
+    source = urllib.request.urlopen(URL).read()
     soup = bs.BeautifulSoup(source, 'lxml')
 
-    print(soup.text)
-    # print(soup.find_all('div', class_='table_wrapper table_controls'))
+    meta = str(soup.find_all('div', class_='players')[0])
+    pos_index = meta.index("Position")
+    position = meta[pos_index + 19: pos_index + 21]
+    print(position)
+    
+    # print(soup.find_all('div', class_='table_outer_container'))
+    # print(soup.find_all('tr', class_='full_table'))
 
 
-get_player_stats("Tom", "Brady")
+get_player_stats("Tom Brady")
+get_player_stats("Jason Kelce")
+
+
